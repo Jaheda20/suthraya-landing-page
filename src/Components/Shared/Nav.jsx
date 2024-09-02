6633923cd3c0f3ee4f148fd055c35b81d2642ca7
Jaheda20 <jahedasultana20@gmail.com>
2024-09-02 13:41:26 +0200

    Designed a basic navbar

--- src/Components/Shared/Nav.jsx
+++ src/Components/Shared/Nav.jsx
@@ -1,9 +1,37 @@
+import { NavLink } from 'react-router-dom';
+import './navbar.css';
+import { MdOutlineLogin } from "react-icons/md";
+
+
 
 const Nav = () => {
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+        <nav className="navbar">
+            <div className="nav-container">
+                <a href="#" className="logo">SuthRaya</a>
+                <ul className="nav-links">
+                    <NavLink>
+                        <li>Home</li>
+                    </NavLink>
+                    <NavLink>
+                        <li>About Us</li>
+                    </NavLink>
+                    <NavLink>
+                        <li>Courses</li>
+                    </NavLink>
+                    <NavLink>
+                        <li>Contact</li>
+                    </NavLink>
+
+                </ul>
+                <a href="" className="btn login-btn">
+                    <MdOutlineLogin className='login-icon'/>
+                    <span>
+                        Login
+                    </span>
+                </a>
+            </div>
+        </nav>
     );
 };
 
